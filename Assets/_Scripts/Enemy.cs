c2b2bb6c1dab44376fe337e3347a8ce53db731bf
emmanuelsalas <eas282@scarletmail.rutgers.edu>
2016-10-27 18:21:09 -0400

    Enemy_0Done

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public float speed = 10f;
+	public float fireRate = 0.3f;
+	public float health = 10;
+	public int score = 100;
+
+	public bool ______________;
+
+	public Bounds bounds; 
+	public Vector3 boundsCenterOffset;
+
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+	public virtual void Move(){
+		Vector3 tempPos = pos;
+		tempPos.y -=speed * Time.deltaTime;
+		pos = tempPos;
+}
+	public Vector3 pos{
+		get{
+			return (this.transform.position);
+		}
+		set {
+			this.transform.position = value;
+		}
+	}
+}
--- Assets/_Scripts/Utils.cs
+++ Assets/_Scripts/Utils.cs
@@ -2,141 +2,117 @@
 using System.Collections;
 using System.Collections.Generic;
 
-//This is actually outside of the UTILS class
 public enum BoundsTest{
 	center,
 	onScreen,
-	offScreen,
+	offScreen
 }
 
 public class Utils : MonoBehaviour {
-	// ========================= Bounds Functions =============================
 
-	//Creates bounds that encapsulate the two Bounds passed in.
-	public static Bounds BoundsUnion(Bounds b0, Bounds b1){
-		//If the size of one of the bounds is Vector3.zero, ignore that one
-		if (b0.size == Vector3.zero && b1.size != Vector3.zero) {
+	//===============Bounds Functions==============
+
+	public static Bounds BoundsUnion (Bounds b0, Bounds b1){
+		if (b0.size == Vector3.zero && b1.size!=Vector3.zero){
 			return (b1);
-		} else if (b0.size != Vector3.zero && b1.size == Vector3.zero) {
+		} else if (b0.size!= Vector3.zero && b1.size!=Vector3.zero){
 			return (b0);
-		} else if (b0.size == Vector3.zero && b1.size == Vector3.zero) {
-			return (b0);
+		}else if (b0.size == Vector3.zero && b1.size == Vector3.zero) {
+			return(b0);
 		}
-		//Stretch b0 to include the b1.min and b1.max
-		b0.Encapsulate (b1.min);
+		//Stretch b0 to include the b1.min and b1 Max
+		b0.Encapsulate(b1.min);
 		b0.Encapsulate (b1.max);
-		return(b0);
+		return (b0);
 	}
 
-	public static Bounds CombineBoundsOfChildren(GameObject go){
+	public static Bounds CombineBoundsOfChildren (GameObject go){
 		//Create an empty bounds b
 		Bounds b = new Bounds(Vector3.zero, Vector3.zero);
-		//If this gameObject has a renderer component
 		if (go.GetComponent<Renderer>() != null){
-			//expand b to contain the renderer's bounds
-			b = BoundsUnion(b, go.GetComponent<Renderer>().bounds);
+			b = BoundsUnion (b, go.GetComponent<Renderer> ().bounds);
 		}
-		//If this GameObject has a Collider Component...
 		if (go.GetComponent<Collider>() != null){
-			//Expand b to contain the Collider's Bounds
-			b = BoundsUnion(b, go.GetComponent<Collider>().bounds);
+			b = BoundsUnion (b, go.GetComponent<Collider> ().bounds);
 		}
-		//Recursively iterate through each child of this gameObject.transform
-		foreach(Transform t in go.transform){
-			//Expand b to contain their Bounds as well
-			b = BoundsUnion(b,CombineBoundsOfChildren(t.gameObject));
+		foreach (Transform t in go.transform){
+			b = BoundsUnion (b, CombineBoundsOfChildren (t.gameObject));
 		}
-		return(b);
+
+		return (b);
 	}
 
 	//Make a static read-only public property camBounds
-	static public Bounds camBounds{
+	static public Bounds cambounds {
 		get{
-			//if _camBounds hasn't been set yet
 			if (_camBounds.size == Vector3.zero){
-				//SetCameraBounds using the default Camera
-				SetCameraBounds();
+				SetCameraBounds ();
 			}
-				return(_camBounds);
-			}
+			return(_camBounds);
 		}
+	}
+
 	//This is the private static field that camBounds uses
+
 	static private Bounds _camBounds;
 
-	//This function is used by camBounds to set _camBounds and can also be called directly
-		public static void SetCameraBounds (Camera cam=null){
-			//If no Camera was pass in, use the main Camera
-			if (cam == null) cam = Camera.main;
-			//This makes a couple of important assumptions about the Camera
-			//1. The Camera is Orthopedic 2. The camera is at a rotation of R:[0,0,0]
+	public static void SetCameraBounds(Camera cam=null){
+		if (cam == null) cam = Camera.main;
 
-			//Make the Vector3s at the topLeft and BottomRight of the Screen coords
-			Vector3 topLeft = new Vector3(0,0,0);
-			Vector3 bottomRight = new Vector3(Screen.width, Screen.height,0);
+		Vector3 topLeft = new Vector3 (0, 0, 0);
+		Vector3 bottomRight = new Vector3 (Screen.width, Screen.height, 0);
 
-			//Convert these to world coordinates
-			Vector3 boundTLN = cam.ScreenToWorldPoint(topLeft);
-			Vector3 boundBRF = cam.ScreenToWorldPoint(bottomRight);
+		Vector3 boundTLN = cam.ScreenToWorldPoint (topLeft);
+		Vector3 boundBRF = cam.ScreenToWorldPoint (bottomRight);
 
-			//Adjust their za to be at the near and far camera clipping planes
-			boundTLN.z += cam.nearClipPlane;
-			boundBRF.z += cam.farClipPlane;
+		boundTLN.z += cam.nearClipPlane;
+		boundBRF.z += cam.farClipPlane;
 
-			//Find the center of the Bounds
-			Vector3 center = (boundTLN + boundBRF)/2f;
-			_camBounds = new Bounds(center, Vector3.zero);
-			//Expand _camBounds to encapsulate the extents.
-			_camBounds.Encapsulate(boundTLN);
-			_camBounds.Encapsulate(boundBRF);
-		}
-			
+		Vector3 center = (boundTLN + boundBRF) / 2f;
+		_camBounds = new Bounds (center, Vector3.zero);
+		//Expand _camBounds to encapsulate the extents
+		_camBounds.Encapsulate (boundTLN);
+		_camBounds.Encapsulate (boundBRF);
+	}
 
-		//Checks to see whether the bounds bnd are within the camBounds
-		public static Vector3 ScreenBoundsCheck(Bounds bnd, BoundsTest test = BoundsTest.center){
-			return (BoundsInBoundsCheck(camBounds, bnd, test));
-		}
+	//Test to see whether boudns are on screen
+	public static Vector3 ScreenBoundsCheck(Bounds bnd, BoundsTest test = BoundsTest.center){
+		return (BoundsInBoundsCheck ( _camBounds, bnd, test));
+	}
 
-		//Checks to see whether Bounds lilB are within Boungs bigB
-		public static Vector3 BoundsInBoundsCheck(Bounds bigB, Bounds lilB, BoundsTest test = BoundsTest.onscreen){
-			//The behavior of this function is different based on the BoundsTest that has been selected
-
-			//Get the center of lilB
+		public static Vector3 BoundsInBoundsCheck ( Bounds bigB, Bounds lilB, BoundsTest test = BoundsTest.onScreen){
 			Vector3 pos = lilB.center;
-
-			//Initalize the offset at [0.0.0]
+			//Get the center of lilB
 			Vector3 off = Vector3.zero;
-
-			switch (test){
-			//The center test determines what off (offset would have to be applied to lilB to move its center back inside bigB
+			//Initalize the offset at [0,0,0]
+			switch(test){
+			//The center test determins what off (offset) would have to be applied to lil B tomove its center back inside BigB
 			case BoundsTest.center:
-			if (bigB.Contains(pos)){
-				return(Vector3.zero);
-			}
-
-			if (pos.x>bigB.max.x){
-				off.x = pos.x - bigB.max.x;
-			} else if (pos.x<bigB.min.x){
-				off.x=pos.x - bigB.min.x;
-			}
-			if (pos.y> bigB.max.y){
-				off.y = pos.y - bigB.max.y;
-			} else if (pos.y<bigB.min.y){
-				off.y = pos.y - bigB.min.y;
-			}
-			if (pos.z > bigB.max.z){
-				off.z = pos.z - bigB.max.z;
-			} else if (pos.z < bigB.min.z){
-				off.z = pos.z - bigB.min.z;
-			}
-			return(off);
-
-			//The oncreeen test determines what off would have to be applied to keep all of lilB inside big B
-
-			case BoundsTest.onscreen:
-				if (bigB.Contains(lilB.min) && bigB.Contains(lilB.max)){
+				if (bigB.Contains(pos)){
 					return(Vector3.zero);
 				}
+				//if not contained, find the offset
+				if (pos.x > bigB.max.x){
+					off.x = pos.x - bigB.max.x;
+				} else if (pos.x < bigB.min.x){
+					off.x = pos.x - bigB.min.x;
+				}
+				if (pos.y > bigB.max.y){
+					off.y = pos.y - bigB.max.y;
+				} else if (pos.y < bigB.min.y){
+					off.y = pos.y - bigB.min.y;
+				}
+				if (pos.z > bigB.max.z){
+					off.z = pos.z - bigB.max.z;
+				} else if (pos.z < bigB.min.z){
+					off.z = pos.z - bigB.min.z;
+				}
+				return (off);
 
+			case BoundsTest.onScreen:
+				if (bigB.Contains(lilB.min) && bigB.Contains(lilB.max)){
+					return(Vector3.zero);
+					}
 				if (lilB.max.x > bigB.max.x){
 					off.x = lilB.max.x - bigB.max.x;
 				} else if (lilB.min.x < bigB.min.x){
@@ -154,13 +130,14 @@ public class Utils : MonoBehaviour {
 				}
 				return (off);
 
-				case BoundsTest.offScreen:
+				//The offscreen test determines what off owuld need to be applid to move any tiny part of lil b inside of big B
+			case BoundsTest.offScreen:
 				bool cMin = bigB.Contains(lilB.min);
 				bool cMax = bigB.Contains(lilB.max);
-				if (cMin || cmax){
+				if (cMin || cMax){
 					return (Vector3.zero);
 				}
-
+				//if not find the offset
 				if (lilB.min.x > bigB.max.x){
 					off.x = lilB.min.x - bigB.max.x;
 				} else if (lilB.max.x < bigB.min.x){
@@ -173,12 +150,13 @@ public class Utils : MonoBehaviour {
 				}
 				if (lilB.min.z > bigB.max.z){
 					off.z = lilB.min.z - bigB.max.z;
-				} else if (lilB.max.z<bigB.min.z){
+				} else if (lilB.max.z < bigB.min.z){
 					off.z = lilB.max.z - bigB.min.z;
 				}
 				return(off);
-			}
-			return(vector3.zero);
-		}
 
-	}
+			}
+
+			return (Vector3.zero);
+		}
+}

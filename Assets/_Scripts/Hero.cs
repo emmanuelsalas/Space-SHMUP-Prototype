0654167e9022181c80655d9a71c54df906a62caa
emmanuelsalas <eas282@scarletmail.rutgers.edu>
2016-11-06 22:24:29 -0500

    Error
    Everything worked fine until I duplicated the 4 other weapons. Hero
    won’t move and error occurs

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -15,6 +15,9 @@ public class Hero : MonoBehaviour {
 	[SerializeField]
 	private float _shieldLevel = 1;
 
+	// Weapon fields
+	public Weapon[] weapons;
+
 	public bool ______________;
 
 	public Bounds bounds;
@@ -27,6 +30,10 @@ public class Hero : MonoBehaviour {
 	void Awake() {
 		S = this; //Set the singleton
 		bounds=Utils.CombineBoundsOfChildren(this.gameObject);
+
+		// Reset the weapons to start _Hero with 1 blaster 
+		ClearWeapons();
+		weapons[0].SetType(WeaponType.blaster);
 	}
 
 	// Use this for initialization
@@ -86,12 +93,56 @@ public class Hero : MonoBehaviour {
 				shieldLevel--;
 				// Destroy the enemy
 				Destroy (go);
+			} else if (go.tag == "PowerUp") {
+				// If the shield was triggerd by a PowerUp
+				AbsorbPowerUp(go);
 			} else {
 				print ("Triggered: " + go.name); 
 			}
 		} else {
 		// Otherwise announce the original other.gameObject
 		print("Triggered: "+other.gameObject.name); // Move this line here! }
+		}
+	}
+
+	public void AbsorbPowerUp( GameObject go ) {
+		PowerUp pu = go.GetComponent<PowerUp>();
+		switch (pu.type) {
+		case WeaponType.shield: // If it's the shield
+			shieldLevel++;
+			break;
+
+		default: // If it's any Weapon PowerUp 
+			// Check the current weapon type 
+			if (pu.type == weapons[0].type) {
+			// then increase the number of weapons of this type
+			Weapon w = GetEmptyWeaponSlot(); // Find an available weapon 
+				if (w != null) {
+					// Set it to pu.type
+					w.SetType(pu.type);
+				}
+	} else {
+		// If this is a different weapon 
+				ClearWeapons();
+				weapons[0].SetType(pu.type);
+			} 
+			break;
+		}
+		pu.AbsorbedBy( this.gameObject );
+	}
+
+	Weapon GetEmptyWeaponSlot() {
+		for (int i=0; i<weapons.Length; i++) {
+			if ( weapons[i].type == WeaponType.none ) {
+				return( weapons[i] );
+			}
+		}
+		return( null );
+	}
+
+	void ClearWeapons() {
+		foreach (Weapon w in weapons) {
+			w.SetType(WeaponType.none);
 		}
 	}
 
